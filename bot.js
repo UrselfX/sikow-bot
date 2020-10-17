bc990537ae690b2aa79b8d5db71c5e9e82399099
UrselfX <70910115+UrselfX@users.noreply.github.com>
2020-10-17 16:03:25 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const prefix = '_'
+
+const token = 'NzY1NjEwNTY5Nzc2MzY1NTk4.X4XUaA.9eDOSqFZK_2QA9RBzdCyoym9tho';
+
+const cheerio = require('cheerio');
+
+const request = require ('request');
+
+const fs = require ('fs');
+
+client.login(procces.env.BOT_TOKEN);
